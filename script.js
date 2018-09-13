6d3d5f8e9b2d2058ec1e8a89c182ac0644a412c7
Hovhannisian <43172747+Hovhannisian@users.noreply.github.com>
2018-09-13 22:09:25 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const num = 5;
+const name = "Someone";
+
+ if(num<5) {
+	console.log("you've wasted less than 5mins");
+ }else if(3 + num !==8){
+  console.log("this is not gonna work");
+ }else {
+	console.log("now you've wasted more than 5mins");
+ }
+console.log(365*5/60 +"hours you'll waste from your whole life if you check this website daily")
+
